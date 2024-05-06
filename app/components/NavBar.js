580893e8a4f11e47f1bd07321237fa6879d2aa7c
constantino <44826802+msconstantino@users.noreply.github.com>
2024-05-06 13:02:21 -0700

    changes for hero banner

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,12 +1,13 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import Stack, { onEntryChange } from "../../lib/index";
-import { Dialog } from "@headlessui/react";
+import { Dialog, Popover, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
   XMarkIcon,
   InformationCircleIcon,
 } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -15,7 +16,7 @@ export default function NavBar() {
 
   const getContent = async () => {
     const entry = await Stack.getElementWithRefs(
-      "blt72f3cc1b5ff28c4a",
+      "blt6d48f1fff86e1e1a",
       "menu",
       [""]
     );
@@ -37,7 +38,6 @@ export default function NavBar() {
       <nav className="flex items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
-
             {/* ----- LOGO IMAGE SERVED FROM CONTENTSTACK ----- */}
             <img
               className="h-8 w-auto"
@@ -45,19 +45,18 @@ export default function NavBar() {
               alt=""
               {...entry?.$?.logo}
             />
-
           </a>
         </div>
         <div className="flex lg:hidden">
           <button
-            type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" 
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={() => setMobileMenuOpen(true)}
           >
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-
           {/* ----- ITTERATE OVER ARRAY FROM CONTENTSTACK ----- */}
           {entry.menu_items?.map((item) => (
             <a
@@ -69,16 +68,13 @@ export default function NavBar() {
               {item.menu_item.title}
             </a>
           ))}
-
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a
             href="https://www.contentstack.com/docs/developers"
             target="__blank"
           >
-            <InformationCircleIcon
-              className="h-6 w-6 flex-none text-neutral-800"
-            />
+            <InformationCircleIcon className="h-6 w-6 flex-none text-neutral-800" />
           </a>
         </div>
       </nav>
@@ -91,7 +87,6 @@ export default function NavBar() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-
             {/* ----- LOGO FROM CONTENTSTACK FOR MOBILE MENU ----- */}
             <a href="#" className="-m-1.5 p-1.5">
               <img className="h-8 w-auto" src={entry?.logo?.url} alt="" />
@@ -107,7 +102,6 @@ export default function NavBar() {
           <div className="mt-6 flow-root">
             <div className="-my-6 ">
               <div className="space-y-2 py-6">
-
                 {/* ----- ITTERATE OVER ARRAY FROM CONTENTSTACK FOR MOBILE MENU ----- */}
                 {entry?.menu_item?.map((item) => (
                   <a
--- app/page.js
+++ app/page.js
@@ -9,11 +9,13 @@ import ImageGallery from "./components/ImageGallery";
 
 export default function Home({ params }) {
   const [entry, setEntry] = useState({});
+  const [banners, setBanners] = useState([]);
+  const [province, setProvince] = useState("");
   const [loading, SetLoading] = useState(true);
 
   const getContent = async () => {
     const entry = await Stack.getElementWithRefs(
-      "blta86813889dab88d1",
+      "bltcaca00efbb33dc3e",
       "home_page",
       ["hero_banner", "page_content.image_gallery.gallery_item.page"]
     );
@@ -22,19 +24,82 @@ export default function Home({ params }) {
     SetLoading(false);
   };
 
+  const getBanners = async () => {
+    const banners = await Stack.getAllEntriesByType("hero_banner");
+    // console.log("homepage:", entry);
+    setBanners(banners);
+    SetLoading(false);
+  };
+
   useEffect(() => {
     onEntryChange(getContent);
+    onEntryChange(getBanners);
   }, []);
 
   if (loading) {
     return;
   }
 
+  console.log(banners);
+
+
+
+  function ProvinceSelector() {
+    const notificationMethods = [
+      { id: "", title: "Default" },
+      { id: "ontario", title: "Ontario" },
+      { id: "quebec", title: "Quebec" },
+    ];
+
+    return (
+      <div className="flex flex-row justify-center">
+        <fieldset className="m-4">
+          <legend className="sr-only">Notification method</legend>
+          <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+            {notificationMethods.map((notificationMethod) => (
+              <div key={notificationMethod.id} className="flex items-center">
+                <input
+                  id={notificationMethod.id}
+                  name="notification-method"
+                  type="radio"
+                  defaultChecked={notificationMethod.id == province}
+                  className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  onClick={(e) => {setProvince(notificationMethod.id)}}
+                />
+                <label
+                  htmlFor={notificationMethod.id}
+                  className="ml-3 block text-sm font-medium leading-6 text-gray-900"
+                >
+                  {notificationMethod.title}
+                </label>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+      </div>
+    );
+  }
+
+  function BannerWrapper() {
+    if (province === "") {
+      return <Hero content={entry.hero_banner[0]} />;
+    } else {
+      const result = banners[0].filter(
+        banner => !!banner.taxonomies
+      ).filter((banner) => banner.taxonomies[0].term_uid == province);
+
+      console.log(result)
+      return <Hero content={result[0]} />
+    }
+  }
+
   return (
     <>
       <NavBar />
 
-      <Hero content={entry.hero_banner[0]} />
+      <ProvinceSelector />
+
+      <BannerWrapper />
 
       {entry.page_content?.map((item, index) => {
         if (item.hasOwnProperty("text_block")) {
