00f31eaca251fda5a6d76836295d4b19ecf3e481
constantino <44826802+msconstantino@users.noreply.github.com>
2024-05-06 11:44:53 -0700

    init

---
+++ app/page.js
@@ -0,0 +1,51 @@
+"use client";
+import { useState, useEffect } from "react";
+import Stack, { onEntryChange } from "../lib/index";
+import Footer from "./components/Footer";
+import TextBlock from "./components/TextBlock";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import ImageGallery from "./components/ImageGallery";
+
+export default function Home({ params }) {
+  const [entry, setEntry] = useState({});
+  const [loading, SetLoading] = useState(true);
+
+  const getContent = async () => {
+    const entry = await Stack.getElementWithRefs(
+      "blta86813889dab88d1",
+      "home_page",
+      ["hero_banner", "page_content.image_gallery.gallery_item.page"]
+    );
+    // console.log("homepage:", entry);
+    setEntry(entry);
+    SetLoading(false);
+  };
+
+  useEffect(() => {
+    onEntryChange(getContent);
+  }, []);
+
+  if (loading) {
+    return;
+  }
+
+  return (
+    <>
+      <NavBar />
+
+      <Hero content={entry.hero_banner[0]} />
+
+      {entry.page_content?.map((item, index) => {
+        if (item.hasOwnProperty("text_block")) {
+          return <TextBlock key={index} content={item.text_block} />;
+        }
+        if (item.hasOwnProperty("image_gallery")) {
+          return <ImageGallery key={index} content={item.image_gallery} />;
+        }
+      })}
+
+      <Footer />
+    </>
+  );
+}
